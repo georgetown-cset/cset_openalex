cc67299459b26ceb1011591b02976267a66ce6fa
Jennifer Melot <jtmelot@gmail.com>
2023-12-14 17:14:35 -0500

    Add initial version of query

---
+++ sql/metadata.sql
@@ -0,0 +1,97 @@
+create or replace table cset_openalex.metadata AS 
+with
+field_name_scores AS (
+  SELECT
+    merged_id,
+    name,
+    field.score AS score
+  FROM
+    fields_of_study_v2.field_scores
+  CROSS JOIN
+    UNNEST(fields) AS field
+  LEFT JOIN
+    fields_of_study_v2.field_meta
+    ON
+      field_id = field.id
+  WHERE
+    (level = 1)),
+
+field_order AS (
+  SELECT
+    merged_id,
+    name,
+    score,
+    ROW_NUMBER() OVER(PARTITION BY merged_id ORDER BY score DESC) AS row_num
+  FROM
+    field_name_scores),
+
+top_fields AS (
+  SELECT
+    merged_id,
+    ARRAY_AGG(STRUCT(name, score)) AS top_level1_fields
+  FROM
+    field_order
+  WHERE
+    (
+      row_num < 4
+    ) AND (
+      merged_id IN (
+        SELECT merged_id
+        FROM
+          literature.papers
+        WHERE
+          (
+            title_english IS NOT NULL
+          ) AND (abstract_english IS NOT NULL) AND (LENGTH(abstract_english) > 500) AND (year > 2010)
+      )
+    )
+  GROUP BY merged_id
+),
+
+ai_pubs AS (
+  select
+    merged_id,
+    ai_filtered or nlp_filtered or cv_filtered or robotics_filtered as is_ai,
+    nlp_filtered as is_nlp,
+    cv_filtered as is_cv,
+    robotics_filtered as is_robotics
+  from
+    article_classification.predictions
+  where
+    ai_filtered is true 
+       or nlp_filtered is true 
+       or cv_filtered is true 
+       or robotics_filtered is true
+),
+
+ai_safety_pubs AS (
+  select
+    merged_id,
+    preds_str as is_ai_safety
+  from
+    ai_safety_datasets.ai_safety_predictions
+)
+
+select
+  id,
+  top_level1_fields,
+  is_ai,
+  is_nlp,
+  is_cv,
+  is_robotics,
+  is_ai_safety
+from 
+  openalex.works
+inner join
+  literature.sources
+on id = orig_id
+left join
+  top_fields
+using(merged_id) 
+left join
+  ai_pubs
+using(merged_id)
+left join
+  ai_safety_pubs
+using(merged_id)
+
